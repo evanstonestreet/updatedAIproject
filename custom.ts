10405eddcaba5f8347380158392d892aa33a6d3e
evanstonestreet <123288382+evanstonestreet@users.noreply.github.com>
2024-03-20 20:31:48 -0400

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,68 @@
+
+/**
+* Use this file to define custom functions and blocks.
+* Read more at https://makecode.microbit.org/blocks/custom
+*/
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace custom {
+
+    // training object that keeps track of all variables needed for training
+    let training = {
+        status: false, // determines whether or not we are in training mode (as opposed to testing)
+        currentlyRunning: false, // true if we are in the middle of a training session and data is being recorded
+        numOfTrainingSessions: 0, // gives number of training sessions (inputed by user as a block parameter)
+        sessionNum: 0, // keeps track of how many sessions we have trained already
+        dataSessions: [] as number[][], // 2D array that stores each 1D array of data associated with each training session
+        currentSession: [] as number[], // 1D array that temporarily stores data for each session before pushing to dataSessions and clearing itself
+        activity: "", // mo for motor and mu for music
+        moreSessions: function(){ // returns true if we still have more sessions to train
+            return training.sessionNum < training.numOfTrainingSessions
+        }
+    }
+    
+    //% block = "trainingMode"
+    export function trainingMode(): void {
+        training.status = true;
+        serial.writeLine("Training mode turned on")
+    }
+
+    //% block = "trainingButtonA"
+    export function trainingButtonA(): void{
+        if (training.status && !training.currentlyRunning){
+            training.currentlyRunning = true
+            while(training.currentlyRunning && training.moreSessions()){
+                // PLACEHOLDER CODE BELOW, TO BE REPLACED WITH EMG CODE
+                serial.writeLine(input.acceleration(Dimension.X) + " ")
+                training.currentSession.push(input.acceleration(Dimension.X))
+            }
+        }
+    }
+
+    //% block = "trainingButtonB"
+    export function trainingButtonB(): void {
+        if (training.status && training.currentlyRunning) {
+            training.currentlyRunning = false
+            training.dataSessions.push(training.currentSession)
+            training.currentSession = []
+            training.sessionNum += 1
+        }
+    }
+    
+    //% block="Turn motor by %degrees degrees (Train for %numSessions sessions)"
+    export function turnMotor (degrees: number, numSessions: number) {
+        //serial.writeLine("Turning motor by " + degrees + " degrees")
+        training.numOfTrainingSessions = numSessions
+        training.activity = "mo"
+    }
+}
---
+++ main.ts
@@ -0,0 +1,5 @@
+custom.trainingMode()
+custom.turnMotor(0, 0)
+basic.forever(function () {
+	
+})
